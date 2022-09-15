7e9444381323ca8c2a32c750398ae68db35c4a5f
VmTalik <vmarlyan@mail.ru>
2022-09-15 23:27:32 +0300

    Add a route for the data edit page controller

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -4,10 +4,12 @@ from .views import other_page
 from .views import BbLoginView
 from .views import profile
 from .views import BbLogoutView
+from .views import ChangeInfoViewUser
 
 app_name = 'main'
 urlpatterns = [
-    path('accounts/logout',BbLogoutView.as_view(),name='logout'),
+    path('accounts/profile/change/', ChangeInfoViewUser.as_view(), name='profile_change'),
+    path('accounts/logout/', BbLogoutView.as_view(), name='logout'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BbLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
