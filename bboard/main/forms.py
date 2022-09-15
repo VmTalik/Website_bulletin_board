163b4d1628d41c1e068edee0d7b6a5b95306ba31
VmTalik <vmarlyan@mail.ru>
2022-09-15 23:09:53 +0300

    Add a user input form

---
+++ bboard/main/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import AdvUser
+
+
+class ChangeInfoFormUser(forms.ModelForm):
+    email = forms.EmailField(required=True, label='Электронная почта')
+
+    class Meta:
+        model = AdvUser
+        fields = ('username', 'email', 'first_name', 'last_name', 'send_messages')
