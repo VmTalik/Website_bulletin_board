3c7756eeb2ae06bba3210cbddc484a43c7a4c8fd
VmTalik <vmarlyan@mail.ru>
2022-09-15 22:41:47 +0300

    Add routes for login and profile controllers

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from .views import index
 from .views import other_page
 from .views import BbLoginView
+from .views import profile
+from .views import BbLogoutView
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/logout',BbLogoutView.as_view(),name='logout'),
+    path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BbLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
