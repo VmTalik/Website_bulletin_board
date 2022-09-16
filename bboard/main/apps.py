2b6da2099762f98aaf1c1482f1b7c79d6b7539a2
VmTalik <vmarlyan@mail.ru>
2022-09-16 21:52:43 +0300

    Add a signal and a handler to it

--- bboard/main/apps.py
+++ bboard/main/apps.py
@@ -1,4 +1,17 @@
 from django.apps import AppConfig
+from django.dispatch import Signal
+from .utilities import send_activation_notification
+
+#сигнал
+user_registered = Signal(providing_args=['instance'])
+
+
+def user_registered_dispatcher(sender, **kwargs):
+    """Функция обработчик сигнала"""
+    send_activation_notification(kwargs['instance'])
+
+#привязка обработчика к сигналу
+user_registered.connect(user_registered_dispatcher)
 
 
 class MainConfig(AppConfig):
