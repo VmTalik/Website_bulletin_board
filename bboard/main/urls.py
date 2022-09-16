afd502bebbb13546f194db7216dc47ab299e1a4f
VmTalik <vmarlyan@mail.ru>
2022-09-16 22:01:49 +0300

    Add routes for user registration controllers

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -6,9 +6,12 @@ from .views import profile
 from .views import BbLogoutView
 from .views import ChangeInfoViewUser
 from .views import BbPasswordChangeView
+from .views import RegisterViewUser,RegisterViewDone
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/register/done/', RegisterViewDone.as_view(),name = 'register_done'),
+    path('accounts/register/', RegisterViewUser.as_view(),name='register'),
     path('accounts/profile/change/', ChangeInfoViewUser.as_view(), name='profile_change'),
     path('accounts/logout/', BbLogoutView.as_view(), name='logout'),
     path('accounts/password/change/',BbPasswordChangeView.as_view(),name='password_change'),
