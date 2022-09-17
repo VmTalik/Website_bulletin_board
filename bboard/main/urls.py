4ffac23ee78c667ba7cc385dee79c034673aad08
VmTalik <vmarlyan@mail.ru>
2022-09-17 22:13:21 +0300

    Add route for user delete controller

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -8,12 +8,14 @@ from .views import ChangeInfoViewUser
 from .views import BbPasswordChangeView
 from .views import RegisterViewUser,RegisterViewDone
 from .views import user_activate
+from .views import DeleteViewUser
 
 app_name = 'main'
 urlpatterns = [
     path('accounts/register/activate/<str:sign>/',user_activate, name='register_activate'),
     path('accounts/register/done/', RegisterViewDone.as_view(),name = 'register_done'),
     path('accounts/register/', RegisterViewUser.as_view(),name='register'),
+    path('accounts/profile/delete/',DeleteViewUser.as_view(),name= 'profile_delete'),
     path('accounts/profile/change/', ChangeInfoViewUser.as_view(), name='profile_change'),
     path('accounts/logout/', BbLogoutView.as_view(), name='logout'),
     path('accounts/password/change/',BbPasswordChangeView.as_view(),name='password_change'),
