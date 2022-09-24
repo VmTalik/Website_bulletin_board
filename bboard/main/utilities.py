36119e2fc25b6f322e720469ae6da50fe84f831f
VmTalik <vmarlyan@mail.ru>
2022-09-24 13:04:26 +0300

    Add function to generate the names of uploaded files

--- bboard/main/utilities.py
+++ bboard/main/utilities.py
@@ -1,6 +1,8 @@
 from django.template.loader import render_to_string
 from django.core.signing import Signer
 from bboard.settings import ALLOWED_HOSTS
+from os.path import splitext
+from datetime import datetime
 
 signer = Signer()
 
@@ -15,3 +17,8 @@ def send_activation_notification(user):
     subject = render_to_string('email/activation_letter_subject.txt', context)
     body_text = render_to_string('email/activation_letter_body.txt', context)
     user.email_user(subject, body_text)
+
+
+def get_timestamp_path(instance, filename):
+    "Функция, генерирующая имена сохраняемых в модели выгруженных файлов"
+    return '%s%s' % (datetime.now().timestamp(), splitext(filename)[1])
