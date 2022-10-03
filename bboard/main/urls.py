f289b77d344d8c734bb0224e70d10bd84d23e041
VmTalik <vmarlyan@mail.ru>
2022-10-03 18:31:09 +0300

    Add controller route to add ads

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -16,6 +16,7 @@ from .views import PasswordResetViewComplete
 from .views import by_rubric
 from .views import detail
 from .views import profile_bb_detail
+from .views import profile_bb_add
 
 app_name = 'main'
 urlpatterns = [
@@ -26,7 +27,8 @@ urlpatterns = [
     path('accounts/profile/change/', ChangeInfoViewUser.as_view(), name='profile_change'),
     path('accounts/logout/', BbLogoutView.as_view(), name='logout'),
     path('accounts/password/change/', BbPasswordChangeView.as_view(), name='password_change'),
-    path('accounts/profile/<int:pk>/',profile_bb_detail, name ='profile_bb_detail'),
+    path('accounts/profile/add', profile_bb_add, name='profile_bb_add'),
+    path('accounts/profile/<int:pk>/', profile_bb_detail, name='profile_bb_detail'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BbLoginView.as_view(), name='login'),
     path('<int:rubric_pk>/<int:pk>/', detail, name='detail'),
