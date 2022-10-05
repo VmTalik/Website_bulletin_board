8ecd18d889a2331f01c46f47ac7dd85dac820efd
VmTalik <vmarlyan@mail.ru>
2022-10-05 09:44:05 +0300

    Add comment controller function

--- bboard/api/views.py
+++ bboard/api/views.py
@@ -4,6 +4,11 @@ from main.models import Bb
 from .serializers import BbSerializer
 from rest_framework.generics import RetrieveAPIView
 from .serializers import BbDetailSerializer
+from rest_framework.decorators import permission_classes
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from main.models import Comment
+from .serializers import CommentSerializer
 
 
 @api_view(['GET'])
@@ -19,3 +24,22 @@ class BbDetailView(RetrieveAPIView):
     """Класс-контроллер для вывода сведений о выбранном объявлении"""
     queryset = Bb.objects.filter(is_active=True)
     serializer_class = BbDetailSerializer
+
+
+@api_view(['GET', 'POST'])
+@permission_classes((IsAuthenticatedOrReadOnly,))
+def comments(request, pk):
+    """Функция-контроллер, выдает список комментариев и
+    добавляет новый комментарий"""
+    if request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+    else:
+        comments = Comment.objects.filter(is_active=True, bb=pk)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
