818f14b5c07526d800728ca36741662a7db008a4
VmTalik <vmarlyan@mail.ru>
2022-10-05 09:13:05 +0300

    Add a route for the ad details display controller

--- bboard/api/urls.py
+++ bboard/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import bbs
+from .views import BbDetailView
 
 urlpatterns = [
+    path('bbs/<int:pk>/', BbDetailView.as_view()),
     path('bbs/', bbs),
 ]
