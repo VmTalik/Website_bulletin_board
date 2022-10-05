a0c67817b1b2cefd2dbbd3a60d72105fa2c5ace5
VmTalik <vmarlyan@mail.ru>
2022-10-05 09:08:59 +0300

    Add controller class for displaying information about the ad

--- bboard/api/views.py
+++ bboard/api/views.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from main.models import Bb
 from .serializers import BbSerializer
+from rest_framework.generics import RetrieveAPIView
+from .serializers import BbDetailSerializer
 
 
 @api_view(['GET'])
@@ -11,3 +13,9 @@ def bbs(request):
         bbs = Bb.objects.filter(is_active=True)[:10]
         serializer = BbSerializer(bbs, many=True)
         return Response(serializer.data)
+
+
+class BbDetailView(RetrieveAPIView):
+    """Класс-контроллер для вывода сведений о выбранном объявлении"""
+    queryset = Bb.objects.filter(is_active=True)
+    serializer_class = BbDetailSerializer
